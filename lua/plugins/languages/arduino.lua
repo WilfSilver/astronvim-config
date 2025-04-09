31e874c767f335ac0b9e54f857259e7ee89f1fb3
Wilf Silver <git@wilfsilver.co.uk>
2025-04-09 11:44:18 +0100

    Improve arduino plugin configuration

--- lua/plugins/languages/arduino.lua
+++ lua/plugins/languages/arduino.lua
@@ -1,22 +1,34 @@
-local wk = require "which-key"
-
 ---@type LazySpec
 return {
   "stevearc/vim-arduino",
-  ft = "arduino",
-  config = function()
-    -- TODO: update commands to use snacks?
-    wk.add {
-      { "<Leader>a", group = "Arduino" },
-      {
-        mode = "n",
-        { "<Leader>aa", "<cmd>ArduinoAttach<CR>", desc = "Attatch" },
-        { "<Leader>am", "<cmd>ArduinoVerify<CR>", desc = "Verify" },
-        { "<Leader>au", "<cmd>ArduinoUpload<CR>", desc = "Upload" },
-        { "<Leader>ad", "<cmd>ArduinoUploadAndSerial<CR>", desc = "Upload + Serial" },
-        { "<Leader>ab", "<cmd>ArduinoChooseBoard<CR>", desc = "Choose Board" },
-        { "<Leader>ap", "<cmd>ArduinoChooseProgrammer<CR>", desc = "Choose Programmer" },
-      },
-    }
-  end,
+  event = "User AstroFile",
+  cmd = {
+    "ArduinoAttach",
+    "ArduinoVerify",
+    "ArduinoUpload",
+    "ArduinoUploadAndSerial",
+    "ArduinoChooseBoard",
+    "ArduinoChoosePort",
+    "ArduinoChooseProgrammer",
+    "ArduinoInfo",
+  },
+  specs = {
+    {
+      "AstroNvim/astrocore",
+      opts = function(_, opts)
+        local maps = opts.mappings
+
+        local prefix = "<Leader>A"
+        maps.n[prefix] = { desc = "Arduino" }
+
+        maps.n[prefix .. "a"] = { "<cmd>ArduinoAttach<CR>", desc = "Attatch" }
+        maps.n[prefix .. "i"] = { "<cmd>ArduinoInfo<CR>", desc = "Info" }
+        maps.n[prefix .. "m"] = { "<cmd>ArduinoVerify<CR>", desc = "Verify" }
+        maps.n[prefix .. "u"] = { "<cmd>ArduinoUpload<CR>", desc = "Upload" }
+        maps.n[prefix .. "d"] = { "<cmd>ArduinoUploadAndSerial<CR>", desc = "Upload + Serial" }
+        maps.n[prefix .. "b"] = { "<cmd>ArduinoChooseBoard<CR>", desc = "Choose Board" }
+        maps.n[prefix .. "p"] = { "<cmd>ArduinoChooseProgrammer<CR>", desc = "Choose Programmer" }
+      end,
+    },
+  },
 }
